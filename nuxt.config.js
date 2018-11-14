927bdf52cc5ea53cee3c87ff1cb12f7222967b8c
RubystarAshe <rubystarashe@gmail.com>
2018-11-14 19:14:09 +0900

    [C] Nuxt init

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+  router: {
+    base: '/spinel/'
+  },
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#fff' },
+  css: [
+  ],
+  plugins: [
+  ],
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+  build: {
+    extend(config, ctx) {
+      
+    }
+  }
+}

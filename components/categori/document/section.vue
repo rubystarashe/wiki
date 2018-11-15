fe5ee20985d62b2e8a532a0551c404869af3151b
RubystarAshe <rubystarashe@gmail.com>
2018-11-15 16:11:15 +0900

    [C] documentSection component, document page props & computed value

---
+++ components/categori/document/section.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  {{document}}
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'document'
+  ]
+}
+</script>
--- pages/_categori/_document/index.vue
+++ pages/_categori/_document/index.vue
@@ -1,5 +1,24 @@
 <template>
 <section>
-  {{$route.params}}
+  {{categori}}
+  <documentSection v-if="document" :document="document"/>
 </section> 
 </template>
+
+<script>
+import documentSection from '~/components/categori/document/section'
+
+export default {
+  components: {
+    documentSection
+  },
+  computed: {
+    categori() {
+      return this.$route.params.categori
+    },
+    document() {
+      return this.$route.params.document
+    }
+  }
+}
+</script>

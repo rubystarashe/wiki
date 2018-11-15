cd94f1ff0d24e2000aac66ff3ef0782d536d5003
RubystarAshe <rubystarashe@gmail.com>
2018-11-15 20:51:00 +0900

    [M] MD 데이터 본문만 가져오게 변경

--- db/document.js
+++ db/document.js
@@ -4,6 +4,6 @@ __dirname = './db'
 module.exports = uri => {
   const data = fs.readFileSync(__dirname + '/' + uri, 'utf8')
   let column = data.split('\r\n')
-  column.splice(0, 1)
+  column.splice(0, 4)
   return column.join('\r\n')
 }

81a6dfb87dcf7428b4bffc992fbe163a19bf1427
RubystarAshe <rubystarashe@gmail.com>
2018-11-17 07:33:49 +0900

    [M] add bg test

--- components/categori/document/section.vue
+++ components/categori/document/section.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div @click="push(uri)">
+  <div @click="$router.push(uri)">
     {{uri}}
     {{document}}
     {{title}}
@@ -8,8 +8,9 @@
     {{subclass}}
     {{description}}</div>
   <transition name="fade">
-    <div class="md-section" v-if="this.column && this.$route.path === this.uri && this.view">
-      <div v-html="column"/>
+    <div v-if="column">
+      <div class="bg" @click="toMenu"></div>
+      <div class="md-section" v-html="column"/>
     </div>
   </transition>
 </div>
@@ -28,28 +29,23 @@ export default {
   ],
   data() {
     return {
-      column: null,
-      view: false
+      column: null
     }
   },
   methods: {
     checkColumn () {
       if (this.$route.path === this.uri) {
           this.column = require('~/static/db' + this.uri + '.md')
-          this.view = true
-        } else this.view = false
+        } else this.column = null
     },
-    push (uri) {
-      if (this.$route.path !== uri) {
-        this.view = false
-        this.$router.push(uri)
-      }
+    toMenu() {
+      this.column = null
+      this.$router.push('/' + this.categori)
     }
   },
   watch: {
     '$route.path': {
       handler: function (n, p) {
-        console.log(this.uri, this.$route.path)
         this.checkColumn()
       },
       immediate: true
@@ -64,6 +60,18 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+
+.bg {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.md-section {
+  position: fixed;
 }
 
 .md-section>section>:nth-child(1), .md-section>section>:nth-child(2), .md-section>section>:nth-child(3) {
